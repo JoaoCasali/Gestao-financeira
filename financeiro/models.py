c7a392b1d67e74ee7be9d25c3e818c00e9db3edb
João Vitor <joaovitorcasali@gmail.com>
2023-01-20 19:10:04 -0300

    Importando static

--- financeiro/models.py
+++ financeiro/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 # Create your models here.
 
+
 class Usuario(models.Model):
     nome = models.CharField(max_length=250)
     email = models.EmailField(max_length=50)
@@ -18,3 +19,6 @@ class Movimentacao(models.Model):
     data_movimentacao = models.DateField()
     valor = models.DecimalField(max_digits=19, decimal_places=2)
     descricao = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.descricao
