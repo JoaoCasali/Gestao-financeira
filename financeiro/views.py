0df0c4ca09ff7eae517e1b3ef191298cf2911f40
João Vitor <joaovitorcasali@gmail.com>
2023-01-23 20:49:33 -0300

    tela de cadastro

--- financeiro/views.py
+++ financeiro/views.py
@@ -1,21 +1,28 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from .models import Usuario, Movimentacao
 
-
-def listarMovimentacoes(request):
-    return render(request, 'movimentacoes/listar.html')
+from django.views.decorators.csrf import csrf_protect
 
 
-def login(request):
+def Login(request):
     return render(request, 'usuario/login.html')
 
 
-def cadastrarUsuario(request):
-    return render(request, 'usuario/cadastrar.html')
-    
-def movimentacao(request):
+def CadastrarUsuario(request):
+    return render(request, 'usuario/edicao.html')
+
+
+def SalvarUsuario(request):
+    usuario = request.POST.get('usuario')
+    email = request.POST.get('email')
+    print(usuario)
+
+    return redirect("ListarMovimentacao")
+
+
+def ListarMovimentacao(request):
     movimentacoes = Movimentacao.objects.all()
     conteudo = {
         'movimentacoes': movimentacoes
     }
-    return render(request,'movimentacoes/listar.html', conteudo)
+    return render(request, 'movimentacoes/listar.html', conteudo)
