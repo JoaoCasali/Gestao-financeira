9265b12144d8b5ec0756c3e64bdf26b0357f3159
João Vitor <joaovitorcasali@gmail.com>
2023-01-25 18:13:20 -0300

    tudo funcionando

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -3,8 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.Login, name='Login'),
+    path('Logout', views.Logout, name='Logout'),
+    path('Autenticar', views.Autenticar, name='Autenticar'),
+
     path('usuario/cadastrar', views.CadastrarUsuario, name='CadastrarUsuário'),
     path('SalvarUsuario/', views.SalvarUsuario, name='SalvarUsuario'),
-    path('movimentacoes/listar', views.ListarMovimentacao,name='ListarMovimentacao'),
-    path('movimentacoes/cadastrar', views.CadastrarMovimentacao,name='CadastrarMovimentacao'),
+
+    path('movimentacoes/listar', views.ListarMovimentacao,
+         name='ListarMovimentacao'),
+    path('movimentacoes/edicao', views.ChamarTelaCadastrarMovimentacao,
+         name='ChamarTelaCadastrarMovimentacao'),
+    path('movimentacoes/cadastrar', views.CadastrarMovimentacao,
+         name='CadastrarMovimentacao'),
 ]
--- financeiro/views.py
+++ financeiro/views.py
@@ -1,7 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
+from django.contrib import messages
 from .models import Usuario, Movimentacao
-
-from django.views.decorators.csrf import csrf_protect
 
 
 def Login(request):
@@ -10,6 +10,25 @@ def Login(request):
 
 def CadastrarUsuario(request):
     return render(request, 'usuario/edicao.html')
+
+
+def Autenticar(request):
+    nome = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+
+    try:
+        usuario = Usuario.objects.get(nome=nome)
+
+    except:
+        messages.warning(request, 'Usuário não encontrado')
+        return redirect('Login')
+
+    if usuario.senha == senha:
+        request.session['idUsuario'] = usuario.id
+        return redirect('ListarMovimentacao')
+
+    messages.warning(request, 'Senha incorreta')
+    return redirect('Login')
 
 
 def SalvarUsuario(request):
@@ -31,20 +50,52 @@ def SalvarUsuario(request):
 
 
 def ListarMovimentacao(request):
-    movimentacoes = Movimentacao.objects.all()
+    idUsuario = request.session['idUsuario']
+    if idUsuario <= 0:
+        messages.warning(request, 'Sua sessão expirou!')
+        return redirect('Login')
+
+    try:
+        usuario = Usuario.objects.get(pk=idUsuario)
+
+    except:
+        messages.warning(request, 'Usuário não encontrado')
+        return redirect('Login')
+
+    movimentacoes = Movimentacao.objects.all().filter(usuario=usuario)
     conteudo = {
         'movimentacoes': movimentacoes
     }
-    return render(request,'movimentacoes/listar.html', conteudo)
+    return render(request, 'movimentacoes/listar.html', conteudo)
 
-def CadastrarMovimentacao(request):    
-    usuario = request.session['idUsuario']
+
+def ChamarTelaCadastrarMovimentacao(request):
+    idUsuario = request.session['idUsuario']
+    if idUsuario <= 0:
+        messages.warning(request, 'Sua sessão expirou!')
+        return redirect('Login')
+
+    return render(request, 'movimentacoes/edicao.html')
+
+
+def CadastrarMovimentacao(request):
+    idUsuario = request.session['idUsuario']
+    if idUsuario <= 0:
+        messages.warning(request, 'Sua sessão expirou!')
+        return redirect('Login')
+
+    try:
+        usuario = Usuario.objects.get(pk=idUsuario)
+
+    except:
+        messages.warning(request, 'Usuário não encontrado')
+        return redirect('Login')
+
     data_movimentacao = request.POST.get('data_movimentacao')
     valor = request.POST.get('valor')
     descricao = request.POST.get('descricao')
-    
-    movimentacao = Movimentacao()
 
+    movimentacao = Movimentacao()
     movimentacao.usuario = usuario
     movimentacao.data_movimentacao = data_movimentacao
     movimentacao.valor = valor
@@ -52,3 +103,9 @@ def CadastrarMovimentacao(request):
     movimentacao.save()
 
     return redirect("ListarMovimentacao")
+
+
+def Logout(request):
+    request.session['idUsuario'] = 0
+
+    return redirect("Login")
